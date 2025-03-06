9100b879991a18bcfd280491c7f554fc4536d2f4
mpatel184 <mvekariya398@gmail.com>
2025-03-06 22:17:13 +0530

    final commit

---
+++ src/country.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react"
+import axios from "axios";
+import { Card, CardContent, CardMedia } from "@mui/material";
+
+export default function Country(){
+    const [data,setData] = useState([])
+
+    const fetchData = async() => {
+        const response = await axios.get("https://xcountries-backend.azurewebsites.net/all")
+        setData(response.data)
+    }
+
+    useEffect(() => {
+        fetchData()
+    },[])
+    return (
+        <>
+            <div className="container" style={{display:"flex", flexWrap:"wrap", gap:"10px", justifyContent:"center"}}>
+            {
+                data.map((country,index) => {
+                    const {name,flag,abbr} = country
+                    return(
+                        <Card key={abbr ? `${abbr}-${index}` : `country-${index}`} style={{display:"flex",flexDirection:"column",textAlign:"center",justifyContent:"center",alignItems:"center",border:"1px solid black",width:"200px", height:"200px",borderRadius:"5px"}}>
+                            <CardMedia
+                                component="img"
+                                image={flag}
+                                alt={name}
+                                sx={{
+                                    width:"100px",
+                                    height:"100px",
+                                    mt:"10px"
+                                }}
+                            />
+                            <CardContent>
+                                <h3>{name}</h3>
+                            </CardContent>
+                        </Card>
+                    )
+                })
+            }
+            </div>
+        </>
+    )
+}
